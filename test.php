e6eda15a6639aaca931922f9a12c0ee46949a188
ingeniedev <mathieu@ingenie.fr>
2016-03-03 11:24:04 +0100

    Added files via upload

---
+++ test.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ *  TEST DE L'API INGENIE
+ */
+
+require_once 'lib/Ingenie/ApiClientRest/Client.php';
+ 
+define('API_USERNAME','');
+define('API_PASSWORD','');
+define('API_ORGANISME','');
+define('API_PROJET_ID','');
+
+$objApiClient = new Ingenie\ApiClientRest\Client();
+
+//------------------------
+// Connexion à l'API
+//------------------------
+try {
+    $objApiClient->connect(API_USERNAME, API_PASSWORD, API_ORGANISME);
+    echo 'Token : '.$objApiClient->getToken()."\n";
+} catch (\Exception $ex) {
+    echo 'Code : '.$ex->getCode().' / Message :  '.$ex->getMessage()."\n";
+    exit;
+}
+
+//---------------------------------------------------
+// Récupération de toutes les sélections d'un projet
+// sans les objets touristiques
+//---------------------------------------------------
+/*
+try {
+    $reponse = $objApiClient->get('/v2/objets_touristiques/selections?id_projet='.API_PROJET_ID.'&fields=-objets_touristiques');
+    $data = $objApiClient->checkReponse($reponse);
+    print_r($data);
+} catch (\Exception $ex) {
+    echo 'Code : '.$ex->getCode().' / Message :  '.$ex->getMessage()."\n";
+}
+*/
+//----------------------------------------------
+// Récupération des sélections
+// avec uniquent les id d'objets touristiques
+//----------------------------------------------
+
+/*
+try {
+    $reponse = $objApiClient->get('/v2/objets_touristiques/selections?id_projet='.API_PROJET_ID.'&ids=68P,69P&fields=objets_touristiques');
+    $data = $objApiClient->checkReponse($reponse);
+    print_r($data);
+} catch (\Exception $ex) {
+    echo 'Code : '.$ex->getCode().' / Message :  '.$ex->getMessage()."\n";
+}*/
+
+//-------------------------------------------------
+// Récupération des id d'objets d'une sélections
+// avec uniquent les id d'objets touristiques
+//-------------------------------------------------
+/*
+try {
+    $reponse = $objApiClient->get('/v2/objets_touristiques/selections/68P?fields=objets_touristiques');
+    $data = $objApiClient->checkReponse($reponse);
+    print_r($data);
+} catch (\Exception $ex) {
+    echo 'Code : '.$ex->getCode().' / Message :  '.$ex->getMessage()."\n";
+}*/
+
+//----------------------------------------------
+// Récupération des objets modifié depuis 72h 
+// uniquement avec les champs id, libelle
+//----------------------------------------------
+
+try {
+    $reponse = $objApiClient->get('/v2/objets_touristiques?id_projet='.API_PROJET_ID.'&update_from=72h&fields=id,libelle');
+    $data = $objApiClient->checkReponse($reponse);
+    print_r($data);
+} catch (\Exception $ex) {
+    echo 'Code : '.$ex->getCode().' / Message :  '.$ex->getMessage()."\n";
+}
+
+//----------------------------------------------
+// Récupération des objets par un id 
+//----------------------------------------------
+/*
+try {
+    $reponse = $objApiClient->get('/v2/objets_touristiques/H|RLDC');
+    $data = $objApiClient->checkReponse($reponse);
+    print_r($data);
+} catch (\Exception $ex) {
+    echo 'Code : '.$ex->getCode().' / Message :  '.$ex->getMessage()."\n";
+}
+*/
+
